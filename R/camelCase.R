6dac49b22f38a2daebadbbdb5e01dc010f7e03c8
Marcel Schilling <marcel.schilling@mdc-berlin.de>
2017-03-15 13:21:54 +0100

    added camelCase function

---
+++ R/camelCase.R
@@ -0,0 +1,13 @@
+#' camelCase is cool
+#'
+#' This function doesn't like the \code{camel_case_linter} because it's not
+#' called \code{camelCaseLinter}
+#'
+#' @return single-element \code{character} vector with an educated opinon on
+#' camelCase
+#'
+#' @examples
+#' camelCase()
+#'
+#' @export
+camelCase = function() return("cool")
